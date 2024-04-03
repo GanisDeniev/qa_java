27360460f017b5722f142e8777473a0cc9553dc6
Denis Ganiev <densw-gg@yandex.ru>
2024-04-03 12:21:40 +0300

    Lion isolated

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -2,13 +2,11 @@ package com.example;
 
 import java.util.List;
 
-public class Lion{
-
-    boolean hasMane;
-    private Predator predator;
-
-    public Lion(String sex, Predator predator) throws Exception {
-        this.predator = predator;
+public class Lion {
+    private final boolean hasMane;
+    protected Feline feline;
+    public Lion (String sex, Feline feline) throws Exception {
+        this.feline = feline;
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
@@ -19,7 +17,7 @@ public class Lion{
     }
 
     public int getKittens() {
-        return predator.getKittens();
+        return feline.getKittens();
     }
 
     public boolean doesHaveMane() {
@@ -27,6 +25,6 @@ public class Lion{
     }
 
     public List<String> getFood() throws Exception {
-        return predator.eatMeat();
+        return feline.eatMeat();
     }
 }
